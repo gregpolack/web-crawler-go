fafbd75090184827e3b3aa9c1fdacfada52ab08b
GregPolack <grzelock@gmail.com>
2024-09-04 06:47:44 +0200

    expand func for getting URLs from HTML

--- urls_from_html.go
+++ urls_from_html.go
@@ -1,5 +1,47 @@
 package main
 
-func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
+import (
+	"fmt"
+	"net/url"
+	"strings"
 
+	"golang.org/x/net/html"
+)
+
+func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse raw URL: %v", err)
+	}
+
+	htmlReader := strings.NewReader(htmlBody)
+	doc, err := html.Parse(htmlReader)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse HTML body: %v", err)
+	}
+
+	var urls []string
+
+	var f func(*html.Node)
+	f = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "a" {
+			for _, anchor := range node.Attr {
+				if anchor.Key == "href" {
+					href, err := url.Parse(anchor.Val)
+					if err != nil {
+						fmt.Printf("could not parse href: '%v': %v\n", anchor.Val, err)
+						continue
+					}
+
+					resolvedURL := baseURL.ResolveReference(href)
+					urls = append(urls, resolvedURL.String())
+				}
+			}
+		}
+		for child := node.FirstChild; child != nil; child = child.NextSibling {
+			f(child)
+		}
+	}
+	f(doc)
+	return urls, nil
 }
